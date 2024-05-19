b03536650c3652c9257fd04e9c35c868c30312eb
Mudden88 <mudden@live.se>
2024-05-19 22:19:29 +0200

    profile

--- frontend/src/pages/profile/Profile.tsx
+++ frontend/src/pages/profile/Profile.tsx
@@ -1,20 +1,35 @@
 import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFish } from "@fortawesome/free-solid-svg-icons";
 import "./profile.css";
 
 function Profile() {
   const token = localStorage.getItem("isLoggedIn");
-  const [users, setUsers] = useState<userProfile[]>([]);
+
+  const [user, setUser] = useState<userProfile | null>(null);
+  const [catches, setCatches] = useState<UserCatch[] | null>(null);
+  const [add, setAdd] = useState<boolean>(false);
 
   interface userProfile {
+    id: number;
+    user_id: number;
     username: string;
     email: string;
+    account_created: string;
+  }
+
+  interface UserCatch {
+    index: number;
+    id: number;
+    user_id: number;
+    username: string;
     species: string;
     weight: number;
     length: number;
     c_r: number;
-    password: string;
-    token: string;
-    user_id: number;
+    location: string;
+    imgurl: string;
+    catch_created: string;
   }
 
   useEffect(() => {
@@ -22,38 +37,125 @@ function Profile() {
       fetch(`http://localhost:3000/userProfile?token=${token}`)
         .then((response) => response.json())
         .then((result) => {
-          setUsers(result);
+          setUser(result[0]);
         })
         .catch((error) => {
           console.error("Error fetching user profile:", error);
-          setUsers([]);
+          setUser(null);
         });
     }
   }, [token]);
 
-  return (
-    <>
-      <div className='container'>
-        {token ? (
-          users.length > 0 ? (
-            users.map((user, index) => (
-              <div className='user-profile' key={index}>
-                <h2>{user.username}</h2>
-                <p>Email: {user.email}</p>
-                <h3>Dina fångster:</h3>
-                <p>Art: {user.species}</p>
+  useEffect(() => {
+    if (user) {
+      fetch(`http://localhost:3000/userCatches/${user.user_id}?token=${token}`)
+        .then((response) => response.json())
+        .then((result) => {
+          setCatches(result);
+        })
+        .catch((error) => {
+          console.error("Error fetching user catches", error);
+        });
+    }
+  }, [token, user]);
 
-                <p>Vikt: {user.weight}</p>
-              </div>
-            ))
-          ) : (
-            <p>Laddar användare</p>
-          )
+  const handleClick = () => {
+    if (add === false) {
+      setAdd(true);
+    }
+  };
+
+  return (
+    <div className='container-profile'>
+      {token ? (
+        user ? (
+          <>
+            <div className='user-profile'>
+              <FontAwesomeIcon className='fishIcon' icon={faFish} />
+              <span className='userName'>{user.username}</span>
+              <img
+                className='ProfilePic'
+                src='https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg'
+                alt='Profile picture'
+              />
+              <p className='userId'>Användar-ID: 0{user.user_id}</p>
+              <p className='userMail'>
+                Email: {user.email} <br />
+              </p>
+              <p className='userCreated'>
+                Medlem sedan: {user.account_created}
+              </p>
+              <hr />
+              <input
+                className='button-41'
+                type='button'
+                value='Lägg till'
+                onClick={handleClick}
+              />
+            </div>
+            <div className='userCatches'>
+              {catches && catches.length > 0 ? (
+                catches.map((fish) => (
+                  <div key={fish.id} className='catchItem'>
+                    <p className='created'>{fish.catch_created}</p>
+                    {fish.imgurl ? (
+                      <a href={fish.imgurl} target='_blanc'>
+                        <img
+                          className='fishImg'
+                          src={fish.imgurl}
+                          alt='User uploaded fish'
+                        />
+                      </a>
+                    ) : (
+                      <img
+                        className='fishImg'
+                        src='../—Pngtree—vector picture icon_4013511.png'
+                        alt='Image by pngtree.com'
+                        width='180px'
+                      />
+                    )}
+
+                    <div className='fishInfo'>
+                      <table>
+                        <thead>
+                          <tr>
+                            <th>Art</th>
+                            <th>Vikt</th>
+                            <th>Längd</th>
+                            <th>C&R</th>
+                            <th>Plats</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>{fish.species}</td>
+                            <td>{fish.weight} gr</td>
+                            <td>{fish.length} cm</td>
+                            <td>{fish.c_r === 1 ? "Ja" : "Nej"}</td>
+                            <td>{fish.location ? fish.location : "n/a"} </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <p className='noFish'>
+                  Du har inte laddat upp något ännu, tryck på knappen nedan för
+                  att börja logga dina fiskar!
+                </p>
+              )}
+            </div>
+          </>
         ) : (
-          <p>Du behöver logga in för att komma åt den här sidan</p>
-        )}
-      </div>
-    </>
+          <p className='LoadingUser'>Laddar användare</p>
+        )
+      ) : (
+        <p className='error'>
+          Du behöver logga in för att komma åt den här sidan
+        </p>
+      )}
+    </div>
   );
 }
 
